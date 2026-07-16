0b1cb0546cdcf0bdef2259ae6bbfdfd7ad5f1389
Lennart <83641616+lennartstoelting@users.noreply.github.com>
2026-07-16 15:14:00 +0200

    added untested crude implementation of correction step

--- ekf_for_imu.py
+++ ekf_for_imu.py
@@ -75,7 +75,13 @@ class Filter:
         self.cal_total_samples = sample_amount_for_calibration
         self.is_calibrated = False
         self.accel_calibration_buffer = []
-        self.state_x = None
+
+        self.x = None
+        self.P = None
+
+        self.Q = None
+        self.R = None
+
         self.states_history = []
 
     # one row of data at a time, like a real-time loop
@@ -94,46 +100,96 @@ class Filter:
         u_g = gyro * (np.pi / 180.0)
         u_a = accel * constants.g
 
-        f_q = ekf_funcs["f_q"]
-        f_a = ekf_funcs["f_a"]
-
-        # update orientation
-        x_q_new = f_q(
-            self.state_x[0],
-            self.state_x[1],
-            self.state_x[2],
-            self.state_x[3],
+        f_q = ekf_funcs["f_q"](
+            self.x[0],
+            self.x[1],
+            self.x[2],
+            self.x[3],
             u_g[0],
             u_g[1],
             u_g[2],
             dt,
         ).flatten()
-        x_q_normalized = x_q_new / np.linalg.norm(x_q_new)
-        self.state_x[0:4] = x_q_normalized
-
-        # update position and velocity
-        accel_rotated = f_a(
-            self.state_x[0],
-            self.state_x[1],
-            self.state_x[2],
-            self.state_x[3],
+        f_a = ekf_funcs["f_a"](
+            self.x[0],
+            self.x[1],
+            self.x[2],
+            self.x[3],
             u_a[0],
             u_a[1],
             u_a[2],
             constants.g,
         ).flatten()
-        v_current = self.state_x[4:7]
-        p_current = self.state_x[7:10]
 
-        v_new = v_current + (accel_rotated * dt)
-        p_new = p_current + (v_new * dt)
+        # prediction step
 
-        self.state_x[4:7] = v_new
-        self.state_x[7:10] = p_new
-        print(f"new implementation: {[v_new] + [p_new]}")
+        # update orientation
+        self.x[0:4] = self.normalize_quat(f_q)
 
-        # if save_to_csv:
-        self.states_history.append(self.state_x.copy())
+        # update position and velocity
+        v_current = self.x[4:7]
+        p_current = self.x[7:10]
+
+        x_v_new = v_current + (f_a * dt)
+        x_p_new = p_current + (x_v_new * dt)
+
+        self.x[4:7] = x_v_new
+        self.x[7:10] = x_p_new
+
+        # calculate jacobians
+        A = ekf_funcs["A"](
+            self.x[0],
+            self.x[1],
+            self.x[2],
+            self.x[3],
+            self.x[4],
+            self.x[5],
+            self.x[6],
+            self.x[7],
+            self.x[8],
+            self.x[9],
+            u_a[0],
+            u_a[1],
+            u_a[2],
+            u_g[0],
+            u_g[1],
+            u_g[2],
+            dt,
+            constants.g,
+        )
+        H = ekf_funcs["H"](self.x[0], self.x[1], self.x[2], self.x[3], constants.g)
+
+        # update state covariance matrix (currently no noise w so W is just the identity matrix)
+        # P = APA.T + WQW.T
+        self.P = A @ self.P @ A.T + self.Q
+
+        # correction step
+
+        # K = PH(HPH.T + VRV.T)^(-1)
+        # adapt R if we are staying static
+        accel_is_static = np.absolute(np.linalg.norm(accel) - 1) < 0.1
+        gyro_is_static = np.linalg.norm(gyro) < 0.01
+        if accel_is_static and gyro_is_static:
+            R = self.R
+        else:
+            R = np.eye(3) * 1e5
+        # combine it in the correction step
+        kalman_gain = self.P @ H.T @ np.linalg.inv(H @ self.P @ H.T + R)
+        print(kalman_gain.shape)
+        print(f"H: {H.shape}")
+
+        # x = x + K(y - h(x, v))
+        h = ekf_funcs["h"](
+            self.x[0], self.x[1], self.x[2], self.x[3], constants.g
+        ).flatten()
+        self.x = self.x + kalman_gain @ (u_a - h)
+        self.x[0:4] = self.normalize_quat(self.x[0:4])
+
+        # P = (I - KH)P
+        self.P = (np.eye(10) - kalman_gain @ H) @ self.P
+
+        if save_to_csv:
+            self.states_history.append(self.x.copy())
 
         return
 
@@ -159,7 +215,7 @@ class Filter:
         qz /= norm
 
         # Initialize state
-        self.state_x = np.array(
+        self.x = np.array(
             [
                 qw,
                 qx,
@@ -173,7 +229,33 @@ class Filter:
                 0.0,  # Initial positions  (x, y, z)
             ]
         )
-        print(f"-> Orientation Quaternion: {self.state_x[0:4]}")
+        print(f"-> Orientation Quaternion: {self.x[0:4]}")
+
+        # Initialize state covariance matrix
+        # decent certainty for the orientation at the start, high certainty (even smaller number) for the velocity and position
+        orientation_certainty = 0.1
+        vel_pos_certainty = 1e-6  # Almost fully confident in the velocity and position since I define them at the start as ground truth
+        state_certainty = np.concatenate(
+            (np.repeat(orientation_certainty, 4), np.repeat(vel_pos_certainty, 6))
+        )
+        self.P = np.diag(state_certainty)
+
+        # Initialize process noise covariance matrix
+        orientation_noise = 1e-8
+        velocity_noise = 1e-4
+        position_noise = 1e-5
+        process_noise = np.concatenate(
+            (
+                np.repeat(orientation_noise, 4),
+                np.repeat(velocity_noise, 3),
+                np.repeat(position_noise, 3),
+            )
+        )
+        self.Q = np.diag(process_noise)
+
+        # Initialize measurement noise covariance matrix
+        accelerometer_noise = 1e-2
+        self.R = np.diag(np.repeat(accelerometer_noise, 3))
 
         # Free up memory by clearing buffers
         self.accel_calibration_buffer = []
@@ -199,6 +281,10 @@ class Filter:
         ]
         df = pd.DataFrame(self.states_history, columns=columns)
         df.to_csv(filename, index=False)
+
+    @staticmethod
+    def normalize_quat(q):
+        return q / np.linalg.norm(q)
 
 
 if __name__ == "__main__":
